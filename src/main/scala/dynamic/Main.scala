a133eb60bb67c6f100aa9155a92d0f6621ff4d84
FMC <franklin@peeriq.com>
2019-04-20 21:38:18 -0400

    this works

--- src/main/scala/dynamic/Main.scala
+++ src/main/scala/dynamic/Main.scala
@@ -6,7 +6,7 @@ import dynamic.schema.TypeSchema
 @FromSchema("macro-fiddle/src/main/resources/test.json") class Test
 object Main extends App {
 
-  val schema = TypeSchema.fromJson("macro-fiddle/src/main/resources/test.json")
+  val schema = TypeSchema.fromJson("src/main/resources/test.json")
 
   println(schema.fields)
 
