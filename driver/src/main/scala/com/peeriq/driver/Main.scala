b7c2b6f901cd243fdce771c0b70345ae62f84f0a
FMC <franklin@peeriq.com>
2019-04-25 13:12:33 -0400

    add csv parser

--- driver/src/main/scala/com/peeriq/driver/Main.scala
+++ driver/src/main/scala/com/peeriq/driver/Main.scala
@@ -1,24 +1,20 @@
 package com.peeriq.driver
 
-import java.time.LocalDate
-
 import com.peeriq.macros.FromSchema
-import com.peeriq.schema.TypeSchema
+import com.peeriq.schema.CsvSchema
 
-@FromSchema("/home/franklin/Documents/dev/macro-fiddle/driver/src/main/resources/test.json") class Test
+@FromSchema("/home/franklin/Documents/dev/macro-fiddle/driver/src/main/resources/test.csv") class Test
 
 object Main extends App {
 
   println("Inside driver")
 
-  val schema = TypeSchema.fromJson("driver/src/main/resources/test.json")
+  // val schema = JsonSchema.fromJson("driver/src/main/resources/test.json")
+  val schema = CsvSchema.fromCsv("driver/src/main/resources/test.csv")
   println(schema)
 
-  // This doesn't compile (not found: value Test)
-  val t = Test("test-1", 2, LocalDate.of(2019, 4, 22))
-
-  // println(t.param3)
-
-  println(t.param3.toString)
+  val t = Test("test_originator", "test_originator_loan_id")
+  
+  println(t.originator_loan_id.toString)
 
 }
